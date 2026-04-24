5e3b5d8f59cd4f3db401e66df8b49c4615a9ef0e
maaz codes <mianmaazullah207@gmail.com>
2026-04-24 11:29:50 +0500

    add python login code

---
+++ if else.py
@@ -0,0 +1,7 @@
+email=(input("enter your email:"))
+password=(input("enter your password:"))
+if email=="mianmaaz@1" and password=="12345":
+    print("welcome")
+else:
+    print("incorrect credential")
+
