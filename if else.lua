e9fce06d7eb9176eeaa30d1dffb0bb6067513b51
maaz codes <mianmaazullah207@gmail.com>
2026-04-24 20:17:20 +0500

    Delete if else.lua

--- if else.lua
+++
@@ -1,7 +0,0 @@
-email=(input("enter your email:"))
-password=(input("enter your password:"))
-if email=="mianmaaz@1" and password=="12345":
-    print("welcome")
-    else
-        print("incorrect credential")
-
